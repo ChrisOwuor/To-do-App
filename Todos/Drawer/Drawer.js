b465e31b57f5f2c886a1d0701806754d2092e0e4
chrispine owuor <owuorchrispine77@gmail.com>
2024-01-21 23:51:09 +0300

    modified drawer nav

--- Todos/Drawer/Drawer.js
+++ Todos/Drawer/Drawer.js
@@ -2,13 +2,14 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Home from "../Screens/Home";
 import Profile from "../Screens/Profile";
 import Settings from "../Screens/Settings";
+import HomeNavigator from "../Navigation/HomeNavigator";
 
 const Drawer = createDrawerNavigator();
 
 export default function MainDrawer() {
   return (
     <Drawer.Navigator>
-      <Drawer.Screen name="Home" component={<Home />} />
+      <Drawer.Screen name="Home" component={<HomeNavigator />} />
       <Drawer.Screen name="Profile" component={<Profile />} />
       <Drawer.Screen name="settings" component={<Settings />} />
     </Drawer.Navigator>
